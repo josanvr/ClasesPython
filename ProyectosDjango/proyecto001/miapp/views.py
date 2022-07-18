bfcda96b99bd2348abf3e0725fe30fa9eea92296
Andres Vera <j.andresvera11@gmail.com>
2022-07-18 12:13:25 -0500

    Semana 14 - Uso de filtros

--- ProyectosDjango/proyecto001/miapp/views.py
+++ ProyectosDjango/proyecto001/miapp/views.py
@@ -3,24 +3,31 @@ from django.shortcuts import redirect, render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    estudiantes = ['Carlos Ruiz','Jordy Quispe','Oscar Reyes','Antony Vasquez']
+    estudiantes = []
+    return render(request, 'index.html', {
+        'titulo':'Inicio',
+        'mensaje':'Proyecto Web con Django en LP3',
+        'estudiantes': estudiantes
+    })
 
 def saludo(request):
-    return render(request, 'saludo.html')
+    return render(request, 'saludo.html', {
+        'titulo':'Bienvenidos',
+        'nombre_autor':'Jose Andres Vera Rodriguez'
+    })
+
 
 def rango(request):
     a = 10
-    b= 20
-    resultado = f"""
-             <h2> Numeros de [{a},{b}] </h2>
-                  resultado: <br>
-             <ul>
-    """
-    while a<=b:
-        resultado += f"<li> {a} </li>"
-        a+=1
-    resultado +="</ul>"
-    return HttpResponse(resultado)
+    b = 20
+    rango_numeros = range(a,b+1)
+    return render(request, 'rango.html', {
+        'titulo':'Rango',
+        'a':a,
+        'b':b,
+        'rango_numeros': rango_numeros
+    })
 
 def rango2(request, a, b):
     if a>b:
